8445bed1f0be4c0e16023651f289b6276590e106
soarxyn <4369637+soarxyn@users.noreply.github.com>
2026-05-19 12:45:12 +0000

    Deploying to gh-pages from @ soarxyn/soarxyn.github.io@355c495c47a2857770b2a72b2081ddc040a1c4be 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,10 +25,10 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/assets/pdf/cv.pdf";
           },
         },{id: "post-a-test-blogpost",
         
